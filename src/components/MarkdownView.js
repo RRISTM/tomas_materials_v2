250558e23927a14600a8e134d2a0a9585e937573
Radek Ripa <ripa.r@seznam.cz>
2019-04-24 23:22:08 +0200

    clean up

--- src/components/MarkdownView.js
+++ src/components/MarkdownView.js
@@ -9,40 +9,13 @@ export class MarkdownView extends Component {
     super(props);
 
     this.state = { mdContent: null };
-    this.codeRef = React.createRef();
   }
 
   componentWillMount() {
-    console.log('Component will mount');
     fetch(testPath).then((response) => response.text()).then((text) => {
       this.setState({ mdContent: text })
     });
   }
-  componentDidMount() {
-    console.log('Component did mount');
-    // this.highlightCode();
-    // const node = ReactDOM.findDOMNode(this);
-    // console.log(node.querySelectorAll('pre code'));
-    // console.log(this.codeRef);
-    // this.codeRef.querySelectorAll('pre code').forEach((block) => {
-    //   hljs.highlightBlock(block);
-    // });
-
-    const node = ReactDOM.findDOMNode(this);
-    console.log(node);
-    // Get child nodes
-    if (node instanceof HTMLElement) {
-      const child = node.querySelectorAll(' div');
-      console.log(child);
-    }
-  }
-
-  // highlightCode() {
-  //   console.log(this.codeEl);
-  //   this.codeEl.querySelectorAll('pre code').forEach((block) => {
-  //     hljs.highlightBlock(block);
-  //   });
-  // }
 
   render() {
     return (
