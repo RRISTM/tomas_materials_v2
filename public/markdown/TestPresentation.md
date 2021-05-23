44afa1660864787a4d892ee4e6190973afa55e52
Radek Ripa <ripa.r@seznam.cz>
2021-05-23 23:34:40 +0200

    presentation mode working two buttons there previous and next

--- public/markdown/TestPresentation.md
+++ public/markdown/TestPresentation.md
@@ -2,9 +2,30 @@
 Presentation
 ----!
 
-# Slide1
+# Timer
 
-Text
+Timer can be used to create delay between operations. Or to simply measure time.
+
+# Timer create
+
+To create a timer we will used function `tx_timer_create`.
+First function argum,ent is pointer to timer handle `timer_ptr`.
+Second argument is name of timer `my_timer`.
+Third argument is function which will be called when timer expires `my_timer_expire`.
+Forth argument is input value for expire function `0x12`.
+Fifth argument is initial tick value `100`. It is time between first activation and timer expire
+Sisth argument is reschedule tick value `200`. This value is used periodicaly after first initiali tick expire. If 0 timer is in One shot mode.
+Seventh argument is timer autostart. It can be set to `TX_NO_ACTIVATE` or `TX_ACTIVATE`.
+
+# Timer use
+
+To use timer in repetitive mode we set repetition value to value between 0x1 to 0xFFFFFFFF. Then when timer is activated we get first initial tick delay and then periodic repetitive delays.
+
+![Timer use](./img/41.svg)
+
+If is used `TX_ACTIVATE` timer is started by `tx_timer_create`. If `TX_NO_ACTIVATE` is used we must start timer with `tx_timer_activate`.
+
+Example of reperitive timer
 
 # Slide2
 
--- src/components/PresentationView.js
+++ src/components/PresentationView.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import MarkdownView from './MarkdownView';
 import { Box, Fab } from '@material-ui/core';
-import { KeyboardArrowRight } from '@material-ui/icons';
+import { KeyboardArrowRight, KeyboardArrowLeft } from '@material-ui/icons';
 import SwipeableViews from 'react-swipeable-views';
 import { virtualize } from 'react-swipeable-views-utils';
 import { withStyles } from '@material-ui/core/styles';
@@ -10,55 +10,88 @@ import { withStyles } from '@material-ui/core/styles';
 const VirtualizeSwipeableViews = virtualize(SwipeableViews);
 
 const styles = (theme) => ({
-    fab: {
+    fabR: {
         position: 'fixed',
         bottom: theme.spacing(2),
         right: theme.spacing(2),
+    },
+    fabL: {
+        position: 'fixed',
+        bottom: theme.spacing(2),
+        right: theme.spacing(12),
     }
 });
 export class PresentationView extends Component {
     constructor(props) {
         super(props)
 
+        const mdChapterRegex = /(?=#\s.*\r\n\r\n)/g;
+        const emptyLine = /^\r\n\r\n/;
+        let mdChapters = this.props.mdChapters;
+        mdChapters = mdChapters.split(mdChapterRegex);
+
+        if (mdChapters[0].search(emptyLine) >= 0) {
+            mdChapters.shift();
+        }
+        const mdChapterSize = mdChapters.length;
+
         this.state = {
-            slideIndex: 0
+            slideIndex: 0,
+            mdChapters: mdChapters,
+            mdChapterSize: mdChapterSize
         };
 
         this.nextSlide = this.nextSlide.bind(this);
+        this.previousSlide = this.previousSlide.bind(this);
     }
 
     nextSlide() {
-        console.log('nextSlide');
+        let nextSlideIdex = this.state.slideIndex + 1;
+        if (nextSlideIdex >= this.state.mdChapterSize) {
+            console.log('çondition fail');
+            return;
+        }
+        console.log(nextSlideIdex);
         this.setState({
-            slideIndex: this.state.slideIndex + 1
+            slideIndex: nextSlideIdex
+        });
+    }
+
+    previousSlide() {
+        let previousSlide = this.state.slideIndex - 1;
+        if (previousSlide < 0) {
+            previousSlide = 0;
+        }
+        this.setState({
+            slideIndex: previousSlide
         });
     }
 
     render() {
         const { classes } = this.props;
-        const mdChapterRegex = /(?=#\s.*\r\n\r\n)/g;
-        const emptyLine = /^\r\n\r\n/;
-        let mdChapters = this.props.mdChapters;
-        mdChapters = mdChapters.split(mdChapterRegex);
+
         /*check first line */
-        if (mdChapters[0].search(emptyLine) >= 0) {
-            mdChapters.shift();
-        }
-        let separatedMdContent = mdChapters.map(mdPart => (<MarkdownView children={mdPart} enqueueSnackbar={this.props.enqueueSnackbar} mdInfo={this.props.mdInfo} />));
-        const slideRenderer = ({ key, indexSlide }) => {
-            // if (indexSlide != undefined) {
-            return <MarkdownView children={mdChapters[this.state.slideIndex]} enqueueSnackbar={this.props.enqueueSnackbar} mdInfo={this.props.mdInfo} />
-            // }
-        };
+        let separatedMdContent = this.state.mdChapters.map(mdPart => (<MarkdownView children={mdPart} enqueueSnackbar={this.props.enqueueSnackbar} mdInfo={this.props.mdInfo} />));
+        // const slideRenderer = (params) => {
+        //     let { key, index } = params;
+        //     if (index < 0) {
+        //         return <Fragment />;
+        //     }
+        //     if (index >= this.state.mdChapterSize) {
+        //         return <Fragment />;
+        //     }
+        //     return <MarkdownView children={this.state.mdChapters[index]} enqueueSnackbar={this.props.enqueueSnackbar} mdInfo={this.props.mdInfo} />
+        // };
         {/* <MarkdownView children={mdChapters[index]} enqueueSnackbar={this.props.enqueueSnackbar} mdInfo={this.props.mdInfo} /> */ }
-        console.log(mdChapters);
         return (
             <Box justify="flex-start" spacing={0} style={{ padding: 24 }}>
-                <VirtualizeSwipeableViews index={this.state.slideIndex} slideRenderer={slideRenderer} />
-                {/* <SwipeableViews index={index} onChangeIndex={this.handleChangeIndex}>
+                <SwipeableViews index={this.state.slideIndex}>
                     {separatedMdContent}
-                </SwipeableViews> */}
-                <Fab aria-label={'Add'} className={classes.fab} color={'primary'} onClick={this.nextSlide}>
+                </SwipeableViews>
+                <Fab aria-label={'Previous slide'} className={classes.fabL} color={'primary'} onClick={this.previousSlide}>
+                    <KeyboardArrowLeft />
+                </Fab>
+                <Fab aria-label={'Next slide'} className={classes.fabR} color={'primary'} onClick={this.nextSlide}>
                     <KeyboardArrowRight />
                 </Fab>
             </Box>
