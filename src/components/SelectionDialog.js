1fa7627a13ff136fea8add5104e170d0b921da8c
Radek Ripa <ripa.r@seznam.cz>
2021-08-03 23:29:39 +0200

    creating tag selection callback for parent

--- src/components/SelectionDialog.js
+++ src/components/SelectionDialog.js
@@ -1,6 +1,15 @@
 import React, { Component } from 'react';
 
 export class SelectionDialog extends Component {
+    constructor(props) {
+        super(props);
+        this.tagSelected = this.tagSelected.bind(this);
+    }
+
+    /*tag is selected inform parent */
+    tagSelected() {
+        this.props.selectedTagCallback();
+    }
     render() {
         return (
             <div>
