c4c109d4a43668de9eda9875b69ee55247146303
Radek Ripa <ripa.r@seznam.cz>
2021-10-16 14:01:24 +0200

    create config template update gitignore

---
+++ src/webConfig_template.js
@@ -0,0 +1,12 @@
+/**
+ * Use in your project as webConfig.js
+ * Uncoment one option
+ * 
+ * local for testing all inside your computer
+ * githubFetch is fetching content from gihub from github.json file
+ * addressFetch is fetching content from github based on address line
+ */
+
+//export const webVariant = 'local';
+export const webVariant = 'githubFetch';
+//export const webVariant = 'addressFetch';
